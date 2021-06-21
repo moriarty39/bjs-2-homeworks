f93a5f8ef5b4d47c8e580d770c3f57888d21f9a7
moriarty39 <60015324+moriarty39@users.noreply.github.com>
2021-06-21 11:50:27 -0400

    Update task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,22 @@
+'use strict'
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  
+  let D;
+    let root = [];
+    D = Math.pow(b,2) - 4 * a * c;
+    if (D < 0) {
+    ;       
+    } else if (D == 0) {
+        root[0] = (-b + Math.sqrt(D))/(2 * a);
+        // console.log (D);
+    } else if (D > 0) {
+        root[0] = (-b + Math.sqrt(D))/(2 * a);
+        // console.log(root[0]);
+        root[1] = (-b - Math.sqrt(D))/(2 * a);
+        // console.log(root[1]);
+    }
+    return root;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
