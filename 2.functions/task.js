3591da8e4eb904901dd26d5de689663774600028
moriarty39 <60015324+moriarty39@users.noreply.github.com>
2021-06-26 16:56:53 -0400

    Update task.js

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,11 +1,25 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min,max,sum,avg;
+  let min = arr[0];
+  let max = arr[0];
+  let sum = 0;
+  let avg;
 
-  // Ваш код
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    sum += arr[i];
+  }
+
+  avg = (sum / arr.length).toFixed(2);
 
   return { min:min, max:max, avg:avg };
 }
+
 
 // Задание 2
 function worker(arr) {
