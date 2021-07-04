4e490d4d211c6fe758f02df8fbe01bf46669afe3
moriarty39 <60015324+moriarty39@users.noreply.github.com>
2021-07-04 14:00:37 -0400

    Update task.js

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,45 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if(this.marks === undefined){ 
+    this.marks = [mark]; 
+    } else {
+    this.marks.push(mark);
+    }
+}
+
+Student.prototype.addMarks = function (...marks) {
+  if(this.marks === undefined){ 
+    this.marks = [];
+    for (i=0; i < marks.length; i++) {
+      this.marks.push(mark); 
+    }
+   } else {
+      for (i=0; i < marks.length; i++) {
+        this.marks.push(mark); 
+    }
+  }
+}
+
+Student.prototype.getAverage = function (...marks) {
+  let total = 0;
+  for (i=0; i < this.marks.length; i++) {
+      total += this.marks[i]; 
+  }
+
+  return total / this.marks.length;
+}
+
+Student.prototype.excludeStudent = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
