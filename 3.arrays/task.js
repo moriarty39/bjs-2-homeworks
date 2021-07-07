7319378094d40f0b79fb851eeb2764aa03a8cf59
moriarty39 <60015324+moriarty39@users.noreply.github.com>
2021-07-06 23:56:12 -0400

    Update task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,22 @@
 function compareArrays(arr1, arr2) {
   let result;
 
-  // Ваш код
-
+  if (arr1.length === arr2.length && arr1.every((n, i) => n === arr2[i])) {
+    result = true;
+  } else {
+    result = false;
+  }
+  
   return result; // boolean
 }
 
 function advancedFilter(arr) {
   let resultArr;
 
-  // Ваш код
+  resultArr = arr
+    .filter((item) => item > 0)
+    .filter((item) => item % 3 === 0)
+    .map((item) => item * 10);
 
   return resultArr; // array
 }
